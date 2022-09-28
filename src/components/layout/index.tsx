a7a93aacb4bf0347ecb189a30a63d49ebeb90363
huangwantong <huangwantong@escopetech.com>
2022-09-28 10:49:33 +0800

    feat: layout 基础布局

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,6 +1,6 @@
-import { LaptopOutlined, NotificationOutlined, UserOutlined } from '@ant-design/icons';
-import { Breadcrumb, Layout, Menu } from 'antd';
-import React from 'react';
+import { LaptopOutlined, NotificationOutlined, UserOutlined, MenuFoldOutlined, MenuUnfoldOutlined } from '@ant-design/icons';
+import { Breadcrumb, Layout, Menu, Button } from 'antd';
+import React, { useState } from 'react';
 import './index.less'
 const { Header, Content, Sider } = Layout;
 
@@ -20,38 +20,60 @@ const items2 = [UserOutlined, LaptopOutlined, NotificationOutlined].map((icon, i
   };
 });
 
-const AppLayout = () => (
-  <div className='ra-layout'>
-    <Layout style={{ minHeight: '100vh' }}>
-      <Header className="ra-layout-header">
-        
-      </Header>
-      <Layout>
-        <Sider width={200} className="ra-layout-sider">
-          <Menu
-            mode="inline"
-            defaultSelectedKeys={['1']}
-            defaultOpenKeys={['sub1']}
-            style={{
-              height: '100%',
-              borderRight: 0,
-            }}
-            items={items2}
-          />
-        </Sider>
+const AppLayout = () => {
+  const [collapsed, setCollapsed] = useState(false);
+  return (
+    <div className='ra-layout ra-layout-mix'>
+      <Layout style={{ minHeight: '100vh' }}>
+        <Header className="ra-layout-header">
+          <a className='ra-header-logo' href="/">
+            <h1>React Admin</h1>
+          </a>
+        </Header>
         <Layout>
-          <Breadcrumb>
-            <Breadcrumb.Item>Home</Breadcrumb.Item>
-            <Breadcrumb.Item>List</Breadcrumb.Item>
-          </Breadcrumb>
-          <Content className="ra-layout-content">
-            Content
-          </Content>
+          <Sider
+            collapsible
+            collapsed={collapsed}
+            onCollapse={value => setCollapsed(value)}
+            collapsedWidth={48}
+            theme="light"
+            width={208}
+            className="ra-layout-sider"
+            trigger={<Button type="link" icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />} />}
+          >
+            <Menu
+              mode="inline"
+              defaultSelectedKeys={['1']}
+              defaultOpenKeys={['sub1']}
+              style={{
+                height: '100%',
+                borderRight: 0,
+              }}
+              items={items2}
+            />
+          </Sider>
+          <Layout>
+            <div className='ra-layout-breadcrumb'>
+              <Breadcrumb>
+                <Breadcrumb.Item>Home</Breadcrumb.Item>
+                <Breadcrumb.Item>List</Breadcrumb.Item>
+              </Breadcrumb>
+
+              <div className='ra-layout-breadcrumb-heading'>
+                <div className='ra-layout-breadcrumb-heading-left'>
+                  <span className='ra-layout-breadcrumb-heading-title'>页面标题</span>
+                </div>
+              </div>
+            </div>
+
+            <Content className="ra-layout-content">
+              Content
+            </Content>
+          </Layout>
         </Layout>
       </Layout>
-    </Layout>
-  </div>
-  
-);
+    </div>
+  )
+};
 
 export default AppLayout;
